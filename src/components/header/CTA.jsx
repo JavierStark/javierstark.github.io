6b60da34092f0e59fc0eda9a298f45625d378232
JavierStark <javiertorralbocortes@gmail.com>
2024-11-11 08:21:17 +0100

    Update

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CTA = () => {
   return (
     <div className={'cta'}>
-      <a href={'https://resume.io/r/Ve6r9r2Vs'} className={'btn'}>Download CV</a>
+      <a href={'https://drive.google.com/drive/folders/1vO_-guGTUJ_YXeVob-vqQXoEYWkkCD0s?usp=sharing'} className={'btn'}>Download CV</a>
       <a href="#contact" className={'btn btn-primary'}>Contact</a>
     </div>
   );
